ccfb2bce2bda73e7c03acceda56f4f0b3742c142
root <root@ariag25.station>
2013-08-20 09:41:52 +0200

    screen?

--- screen_read.py
+++ screen_read.py
@@ -1,5 +1,5 @@
 import serial
-import lib_screen
+import db_query
  
 ser = serial.Serial(
     port='/dev/ttyS4', 
@@ -10,7 +10,9 @@ ser = serial.Serial(
     bytesize=serial.EIGHTBITS
 )  
 
-def goto_form (ser, index):
+
+
+def goto_form (index):
 	
 	lista = ["\x01","\x0A",chr(index),"\x00","\x00"]
 	ls1 = lista[0]	
@@ -20,7 +22,7 @@ def goto_form (ser, index):
 	for i in range (0, len(lista)):
 		ser.write(lista[i])
 
-def string_write (ser, index, numero_codice):
+def string_write (index, numero_codice):
 	
 	lista = ['\x02', chr(index), '\x01',chr(ord(numero_codice))]
 	ls1 = ls_ser[0]	
@@ -30,7 +32,7 @@ def string_write (ser, index, numero_codice):
 	for i in range (0, len(lista)):
 		ser.write(ls_ser[i])
 		
-def light_led (ser, index, bit):
+def light_led (index, bit):
 	lista = ["\x01","\x0E",chr(index),"\x00",chr(bit)]
 	ls1 = lista[0]	
 	for i in range (1, len(lista)):
@@ -39,7 +41,7 @@ def light_led (ser, index, bit):
 	for i in range (0, len(lista)):
 		ser.write(lista[i])
 	
-def set_temperature (ser, index, val):
+def set_temperature (index, val):
 	lista = ["\x01","\x12",chr(index),"\x00",chr(val)]
 	ls1 = lista[0]	
 	for i in range (1, len(lista)):
@@ -50,7 +52,55 @@ def set_temperature (ser, index, val):
 pacchetto = ""
 lista = []
 
-def function(): 
+
+
+def calcola_checksum (comando, parametro):
+	com = ord(comando)
+	for i in range (0, len(parametro)):
+		com = com ^ ord(parametro[i])
+	com = chr(com)
+	return com
+	
+def verifica (lista):
+	if len(lista) == 6:
+		pwd1 = db_query.read_userCode(1)
+		pwd2 = db_query.read_userCode(2)
+		print pwd1
+		print pwd2
+		lista_control1 = []
+		for i in range (0, len(pwd1)):
+			lista_control1.append(pwd1[i])
+		lista_control2 = []
+		for i in range (0, len(pwd2)):
+			lista_control2.append(pwd2[i])
+			
+		if lista == lista_control1:
+			del lista[0:6]
+			return 1
+							
+		elif lista == lista_control2:		
+			del lista[0:6]
+			return 2
+								
+	else:
+		del lista[0:99]
+		return 3
+		
+def spacchetta(pacchetto):
+	comando = pacchetto[0]
+	parametro = pacchetto[1:-1]
+	checksum = pacchetto[-1]
+	chk = calcola_checksum (comando, parametro)		
+	if chk == checksum:
+		return "OK"
+	else:
+		return "NO"
+	
+
+
+
+
+while True: 
 	s = ser.read(1) 
 	if len(s)>0:	
 		pacchetto="".join([pacchetto,s])
@@ -74,7 +124,7 @@ def function():
 						del lista [-1]
 				else:
 					lista.append (numero_codice)
-					string_write (ser, numero_codice)
+	#				string_write (ser, 1, numero_codice)
 		pacchetto = ""
 	
 ser.close()
